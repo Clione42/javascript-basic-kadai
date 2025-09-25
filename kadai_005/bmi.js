cc50d1ebbad9f5f5c774df854478a02a79048282
Clione42 <kana.ikeda@ypg.jp>
2025-09-25 16:05:13 +0900

    再提出の修正をアップ

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,6 +1,7 @@
 // 変数の代入
-let bodyWeight = "68";
-let bodyHeight = "1.7";
+let bodyWeight = 68;
+let bodyHeight = 1.7;
+let bmiValue = (bodyWeight /(bodyHeight * bodyHeight));
 
 // コンソールへの出力
-console.log(bodyWeight /(bodyHeight * bodyHeight));
+console.log(bmiValue);
